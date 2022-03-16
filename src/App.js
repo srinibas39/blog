a0315fb2edea36a519faab11fe3cb7621603042e
srinibas <srinibaskhuntia39@gmail.com>
2022-03-17 04:04:14 +0530

    searchcontent done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { CardContainer } from './components/CardContainer/CardContainer';
 import { Content } from './components/content/Content';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/header/Header';
+import { SearchContent } from './components/searchContent/SearchContent';
 
 
- const App=()=>{
+const App = () => {
   return <>
-  <Header/>
-  <Content/>
-  <CardContainer/>
-  <Footer/>
+    <Header />
+    {/* <Content/>
+    <CardContainer/> */}
+    <SearchContent />
+    <Footer />
   </>
 }
 
---
+++ src/components/searchContent/SearchContent.js
@@ -0,0 +1,8 @@
+
+import "./SearchContent.css";
+export const SearchContent=()=>{
+    return <div className="search-container">
+        <h1>Search Blogs</h1>
+        <p>Enter keywords to search for Blog e.g javascript</p>
+    </div>
+}
