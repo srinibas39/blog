e0ead9d68d6285acee891afb7c1853b51c96f22c
srinibas <srinibaskhuntia39@gmail.com>
2022-03-20 15:34:57 +0530

    implemented active states in link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,18 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import { UFCsvg } from "../svg/UFCsvg"
 import "./Header.css"
 
 export const Header = () => {
 
+
+
     return <header className="header">
         <UFCsvg />
         <div className="header-box">
-            <Link to="/"><p>Blogs</p></Link>
-            <Link to="/search"><p>Search Blogs</p></Link>
-            <Link to="/about"><p>About</p></Link>
+            <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/"><p>Blogs</p></NavLink>
+            <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/search"><p>Search Blogs</p></NavLink>
+            <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/about"><p>About</p></NavLink>
         </div>
     </header>
 }
